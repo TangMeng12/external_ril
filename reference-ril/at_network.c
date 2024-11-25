4f8e3a4ab85bc65bcc94cb906d68774629355e5b
xujianhang <xujianhang@xiaomi.com>
2024-11-25 13:36:47 +0800

    external/ril: remove the dependency on system binder
    Signed-off-by: xujianhang <xujianhang@xiaomi.com>

--- reference-ril/at_network.c
+++ reference-ril/at_network.c
@@ -22,8 +22,8 @@
 #include <stdio.h>
 #include <sys/cdefs.h>
 
-#include <log/log_radio.h>
 #include <telephony/librilutils.h>
+#include <telephony/ril_log.h>
 
 #include "at_modem.h"
 #include "at_network.h"
