d03f54abf4ae6cd55f92cdf7cffaf47b7692c911
xujianhang <xujianhang@xiaomi.com>
2025-09-25 17:12:18 +0800

    external/ril: fix compile warning
    Signed-off-by: xujianhang <xujianhang@xiaomi.com>

--- reference-ril/at_call.c
+++ reference-ril/at_call.c
@@ -1475,7 +1475,7 @@ static void requestSeparateConnection(void* data, size_t datalen, RIL_Token t)
     // Make sure that party is in a valid range.
     // (Note: The Telephony middle layer imposes a range of 1 to 7.
     // It's sufficient for us to just make sure it's single digit.)
-    if (party <= 0) {
+    if (party <= 0 || party > 9) {
         RLOGE("requestSeparateConnection party：%d is invalid!", party);
         RIL_onRequestComplete(t, RIL_E_GENERIC_FAILURE, NULL, 0);
         return;
